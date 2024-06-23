0c512a18c72a33d9b93b3858234edd53b019522e
minskunn <157802869+minskunn@users.noreply.github.com>
2024-06-23 10:41:12 +0200

    Started HTML structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import Weather from "./Weather";
 import "./index.css";
 
 export default function App() {
   return (
     <div className="Weather app">
-      <h1>weather application</h1>
+      <Weather />
+      <footer>This project was coded by Minna and is open-sourced</footer>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>Oslo</h1>
+      <ul>
+        <li>Sunday 10:38</li>
+        <li>Broken clouds</li>
+      </ul>
+      <div className="row"></div>
+      <div className="col-6">6 C</div>
+    </div>
+  );
+}
